2140e632b1a049e491558196d1ecc99bfd30fd8d
Matias Benedetto <matias.benedetto@gmail.com>
2020-03-21 15:24:15 -0300

    shwoing StartQuizButtons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import { NextPage } from 'next';
-import Layout from '../components/Layout'
-import { useUser, useDispatchUser } from '../context/User'
+import Layout from '../components/Layout';
+import { useUser, useDispatchUser } from '../context/User';
 import userAction from '../context/User/actions';
 import { useDispatchGlossary } from '../context/Glossary'
-import { Difficulty, GlossaryItem } from '../context/Glossary/types';
+import { GlossaryItem } from '../context/Glossary/types';
 import glossaryActions from '../context/Glossary/actions';
-import api from '../api'
-import { useEffect } from 'react'
-import CreateQuiz from '../components/CreateQuiz';
+import api from '../api';
+import { useEffect } from 'react';
 import TextInput from '../components/UserInput';
-
+import StartQuizButtons from '../components/StartQuizButtons';
 
 type HomeProps = {
   glossaryData: GlossaryItem[];
@@ -37,10 +36,7 @@ const Home: NextPage<HomeProps> = (props: HomeProps) => {
     <Layout title="Welcome to the Next Crypto Quiz">
       <h1>Welcome {user.name} to Crypto Quiz!</h1>
       <TextInput onChange={handleUserInputChange} value= {user.name} />
-      <CreateQuiz difficulty={Difficulty.Begginer} />
-      <CreateQuiz difficulty={Difficulty.Intermediate} />
-      <CreateQuiz difficulty={Difficulty.Advanced} />
-      <CreateQuiz difficulty={Difficulty.Uncategorized} />
+      <StartQuizButtons />
     </Layout>
   )
 }
--- pages/quiz/results.tsx
+++ pages/quiz/results.tsx
@@ -1,10 +1,37 @@
+import { useQuiz } from '../../context/Quiz';
+import StartQuizButtons from "../../components/StartQuizButtons";
+import Answer from '../../components/Answer';
+import { useEffect } from 'react';
+import { useDispatchUser } from '../../context/User';
+import userActions from '../../context/User/actions';
+import Layout from '../../components/Layout';
 
 
 function Results() {
+  const quiz = useQuiz();
+  const userDipatch = useDispatchUser();
+  const wellAnswered = quiz.questions.filter(item => item.correctAnswer === item.userAnswer);
+  const wellAnsweredIds = wellAnswered.map(item => item.glossaryId);
+  const points: number = wellAnswered.length;
+
+  useEffect(() => {
+    userDipatch(
+      userActions.setScore(points, '+')
+    )
+    userDipatch(
+      userActions.setCorrectAnswers(wellAnsweredIds)
+    )
+  }, []);
+
   return (
-    <div>
-      Results
-    </div>
+    <Layout>
+      <h1>Results</h1>
+      {quiz.questions.map(item => <Answer question={item} key={item.glossaryId} />)}
+      <div>
+        <h2>Continue Playing</h2>
+        <StartQuizButtons />
+      </div>
+    </Layout>
   )
 }
 
